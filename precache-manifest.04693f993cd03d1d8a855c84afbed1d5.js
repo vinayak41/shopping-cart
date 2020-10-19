ffba32403a1b8cc5775d841a37d3eb249ba067b9
vinayak Khandekar <vinayakkhandekar41@gmail.com>
2020-10-19 22:35:35 +0530

    Updates

---
+++ precache-manifest.04693f993cd03d1d8a855c84afbed1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b30b7363e800d55a30cbec5af013d4c",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "38c05ee14b7c77be7d47",
+    "url": "/shopping-cart/static/css/main.d15e0324.chunk.css"
+  },
+  {
+    "revision": "1b658d7a97bf34815054",
+    "url": "/shopping-cart/static/js/2.79152360.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/shopping-cart/static/js/2.79152360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c05ee14b7c77be7d47",
+    "url": "/shopping-cart/static/js/main.0e79d63f.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
